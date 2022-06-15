ae6bb048a1f3391cdc6970c0420cdab7db35604e
khairullah <irull.borneo@gmail.com>
2022-06-15 13:05:51 +0800

    fix bug item home

--- lib/views/pages/home/home.page.dart
+++ lib/views/pages/home/home.page.dart
@@ -83,92 +83,97 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _myCourseSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        SectionWidget(
-          child: SizedBox(
-            height: SizeConfig.blockSizeVertical! * 25,
-            child: ListView.builder(
-              itemCount: 10,
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => CardSection(
-                margin: index == 0
-                    ? EdgeInsets.only(
-                        right: 10,
-                        left: SizeConfig.blockSizeHorizontal! * 8,
-                        top: 2)
-                    : const EdgeInsets.only(right: 10, top: 2),
-                width: SizeConfig.blockSizeHorizontal! * 75,
+    return Builder(
+      builder: (context) {
+        SizeConfig().init(context);
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SectionWidget(
+              child: SizedBox(
                 height: SizeConfig.blockSizeVertical! * 25,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    CachedImage(
-                      width: double.infinity,
-                      height: SizeConfig.blockSizeVertical! * 15,
-                      borderRadius: BorderRadius.circular(10),
-                      fit: BoxFit.fill,
+                child: ListView.builder(
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => CardSection(
+                    margin: index == 0
+                        ? EdgeInsets.only(
+                            right: 10,
+                            left: SizeConfig.blockSizeHorizontal! * 8,
+                            top: 2)
+                        : const EdgeInsets.only(right: 10, top: 2),
+                    width: SizeConfig.blockSizeHorizontal! * 75,
+                    height: SizeConfig.blockSizeVertical! * 25,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CachedImage(
+                          width: double.infinity,
+                          height: SizeConfig.blockSizeVertical! * 15,
+                          borderRadius: BorderRadius.circular(10),
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(height: 8,),
+                        AutoSizeText("Virtual Reality", style: Theme.of(context).textTheme.headline6, minFontSize: 14, maxLines: 2,),
+                        const SizedBox(height: 8,),
+                        AutoSizeText("Satwick Pachino", style: Theme.of(context).textTheme.caption,minFontSize: 12, maxLines: 3,)
+                      ],
                     ),
-                    const SizedBox(height: 8,),
-                    AutoSizeText("Virtual Reality", style: Theme.of(context).textTheme.headline6, minFontSize: 14, maxLines: 2,),
-                    const SizedBox(height: 8,),
-                    AutoSizeText("Satwick Pachino", style: Theme.of(context).textTheme.caption,minFontSize: 12, maxLines: 3,)
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        SectionWidget(
-          title: "Top Educators",
-          child: SizedBox(
-            height: SizeConfig.blockSizeVertical! * 20,
-            child: ListView.builder(
-              itemCount: 10,
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => CardSection(
-                margin: index == 0
-                    ? EdgeInsets.only(
-                        right: 10,
-                        left: SizeConfig.blockSizeHorizontal! * 8,
-                        top: 2)
-                    : const EdgeInsets.only(right: 10, top: 2),
-                width: SizeConfig.blockSizeVertical! * 17,
+            SectionWidget(
+              title: "Top Educators",
+              child: SizedBox(
                 height: SizeConfig.blockSizeVertical! * 20,
-                child: Stack(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          CachedAvatar(radius: SizeConfig.blockSizeHorizontal! * 6.5,),
-                          const SizedBox(height: 8,),
-                          AutoSizeText("Cristoper Roy", style: Theme.of(context).textTheme.headline6, maxFontSize: 15, minFontSize: 12, maxLines: 2,),
-                          const SizedBox(height: 4,),
-                          AutoSizeText("24 Courses", style: Theme.of(context).textTheme.caption,minFontSize: 12, maxLines: 3,)
-                        ],
-                      ),
+                child: ListView.builder(
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => CardSection(
+                    margin: index == 0
+                        ? EdgeInsets.only(
+                            right: 10,
+                            left: SizeConfig.blockSizeHorizontal! * 8,
+                            top: 2)
+                        : const EdgeInsets.only(right: 10, top: 2),
+                    width: SizeConfig.blockSizeVertical! * 17,
+                    height: SizeConfig.blockSizeVertical! * 20,
+                    child: Stack(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              CachedAvatar(radius: SizeConfig.blockSizeHorizontal! * 6.5,),
+                              const SizedBox(height: 8,),
+                              AutoSizeText("Cristoper Roy", style: Theme.of(context).textTheme.headline6, maxFontSize: 15, minFontSize: 12, maxLines: 2,),
+                              const SizedBox(height: 4,),
+                              AutoSizeText("24 Courses", style: Theme.of(context).textTheme.caption,minFontSize: 12, maxLines: 3,)
+                            ],
+                          ),
+                        ),
+                        const Positioned(
+                          bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Icon(FontAwesomeIcons.angleDown, size: 12,))
+                      ],
                     ),
-                    const Positioned(
-                      bottom: 0,
-                        left: 0,
-                        right: 0,
-                        child: Icon(FontAwesomeIcons.angleDown, size: 12,))
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
 
-      ],
+          ],
+        );
+      }
     );
   }
 }
